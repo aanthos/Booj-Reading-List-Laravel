7241a39c4cf289cf697d8d66f9a4787c7a414356
aanthos <anthos@gmail.com>
2019-08-28 00:47:40 -0600

    Fixed several errors - searches properly

--- client/src/components/search-books/AddBook.vue
+++ client/src/components/search-books/AddBook.vue
@@ -1,12 +1,22 @@
 <template>
     <div id="addbook">
-        
+        <h1>Add Book section</h1>
+        <p>Title goes here: {{ title }}</p>
+        <button>Add Button</button>
     </div>
 </template>
 
 <script>
 export default {
-
+    name: 'addbook',
+    props: { 
+        title: String,
+    },
+    data () { 
+        return {
+            heading: ''
+        }
+    },
 }
 </script>
 
