9b3cee194a211a629e4d1bb47c19ceae45ff2e7c
aanthos <athovla@gmail.com>
2019-09-04 20:12:32 -0600

    Added navbar and started book details

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <header>
+      <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand">Reading List</a>
+      </nav>
+    </header>
+    <div class="header">
+      <h1 id="heading">Reading List</h1>
+      <p id="lead">Search the Open Library database to find books and add them to your book list.</p>
+    </div>
     <div class="container">
-    <div class="row">
-      <div class="col-md-7">
-        <Search/>
-      </div>
-      <div class="col-md-5">
-        <BookList/>
+      <div class="row">
+        <div class="col-md-7">
+          <Search/>
+        </div>
+        <div class="col-md-5">
+          <BookList/>
+        </div>
       </div>
     </div>
+    <div id="drawer">
+      <BookDetails/>
     </div>
   </div>
 </template>
@@ -17,12 +28,14 @@
 <script>
 import BookList from './components/book-list/BookList.vue'
 import Search from './components/search-books/Search.vue'
+import BookDetails from './components/book-details/BookDetails.vue'
 
 export default {
   name: 'app',
   components: {
     Search,
-    BookList
+    BookList,
+    BookDetails
   }
 }
 </script>
@@ -34,11 +47,33 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 .container {
   max-width: 1140px;
   margin: auto;
+}
+
+#drawer {
+  display: flex;
+  min-height: 100%;
+  background-color: green;
+}
+
+.header {
+  max-width: 40rem;
+  margin-left: auto;
+  margin-right: auto;
+  padding-bottom: 50px;
+}
+
+#heading {
+  font-weight: 300;
+}
+
+#lead {
+  color: #6c757d;
+  font-size: 1.2em;
+  font-weight: 300;
 }
 </style>
